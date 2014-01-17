64bdb6bc9b5aa1ffe26b708dbaaacc446edc69d1
karhovkirill <hell.is.for@gmail.com>
2014-01-17 16:07:45 +0200

    init Up classes

---
+++ library/up/core/core.php
@@ -0,0 +1,15 @@
+<?php
+namespace up;
+
+/**
+ * Core class
+ *
+ * @author     Kirill Kirhov
+ * @copyright  Up framework (http://up.singleup.net). singleUp team (www.singleup.net)
+ * @license    http://up.singleup.org/license/ (GNU)
+ */
+
+class core
+{
+
+}
---
+++ library/up/up.php
@@ -0,0 +1,16 @@
+<?php
+require_once dirname(__FILE__) . '/core/core.php';
+
+/**
+ * Main class up
+ *
+ * @author     Kirill Kirhov
+ * @relation   core
+ * @copyright  Up framework (http://up.singleup.net). singleUp team (www.singleup.net)
+ * @license    http://up.singleup.org/license/ (GNU)
+ */
+
+class up extends \up\core
+{
+
+}
