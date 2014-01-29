a4953efd7d3a03cc211bbd7d313fd1a2442aadce
karhovkirill <hell.is.for@gmail.com>
2014-01-29 23:55:16 +0200

    up example

---
+++ library/up/up.example.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'core/core.php';
+
+/**
+ * Main class up
+ *
+ * @author     Kirill Kirhov
+ * @relation   core
+ * @copyright  Up framework (http://up.singleup.net). singleUp team (singleup.net)
+ * @license    http://up.singleup.org/license/ (GNU)
+ */
+
+class up extends \up\core
+{
+
+}
